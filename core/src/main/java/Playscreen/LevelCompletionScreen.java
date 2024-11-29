7238ec7acfbee641a27315a323ba32d80efede1b
NamandeepSing <160481665+NamandeepSing@users.noreply.github.com>
2024-11-29 14:57:44 +0530

    Add files via upload

---
+++ core/src/main/java/Playscreen/LevelCompletionScreen.java
@@ -0,0 +1,44 @@
+package Playscreen;
+
+import com.badlogic.gdx.Screen;
+import com.mygdx.game.Main;
+
+public class LevelCompletionScreen implements Screen {
+    public LevelCompletionScreen(Main game) {
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float v) {
+
+    }
+
+    @Override
+    public void resize(int i, int i1) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
